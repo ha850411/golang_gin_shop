e4ab76ede1a17ef33d7529eeec3d146628f90303
eason <le850411@gmail.com>
2021-09-26 17:15:47 +0800

    製作 header 及 logo

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	serv := gin.Default()
 	serv.LoadHTMLGlob("template/*") // 設定 html template loader
+	serv.Static("/asset", "./asset")
+
+	/* route */
 	serv.GET("/", index)
+	/* route */
 	serv.Run(":8888")
 }
 
@@ -16,5 +20,6 @@ func index(c *gin.Context) {
 	data := map[string]string{
 		"name": "Eason",
 	}
+	c.HTML(200, "header.html", map[string]string{})
 	c.HTML(200, "index.html", data)
 }
